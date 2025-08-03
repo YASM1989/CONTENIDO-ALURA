ce7c00b9e153945b30147656f4e9c41671bdc971
YASM1989 <126576019+YASM1989@users.noreply.github.com>
2025-08-03 15:54:07 -0500

    Continuación paso a paso ejercicio

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-alert ('HOLA MUNDO');
+let numeroSecreto=6;    
+let numeroUsuario = prompt ("Me indicas un número por favor:");
+
+console.log(numeroUsuario);
+
+if(numeroUsuario==numeroSecreto){
+    alert('Acertaste el número');
+}
