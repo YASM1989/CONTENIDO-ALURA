aa7e3958969b1f5eb64438cbcb890b25ffb004cf
YASM1989 <126576019+YASM1989@users.noreply.github.com>
2025-08-05 12:32:17 -0500

    Creación de comentarios con (/* */) o (//  //) y modificacación en la línea 3 solicitamos ahora un número entre 1 y 10

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
+//Comentario 'ESTAS SON LAS VARIABLES//
 let numeroSecreto=6;    
-let numeroUsuario = prompt ("Me indicas un número por favor:");
+let numeroUsuario = prompt ("Me indicas un número entre 1 y 10 por favor:");
 
 console.log(numeroUsuario);
-
+/*Este código realiza 
+la comparación
+*/
 if(numeroUsuario==numeroSecreto){
     alert('Acertaste el número');
 }
