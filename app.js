cdeb50795e907694ef893dde8210542cc0faf2da
YASM1989 <126576019+YASM1989@users.noreply.github.com>
2025-08-07 22:37:08 -0500

    Ingresamos (while) en la línea 4, realizamos una identación o vimos lo que es jerarquía de código. Definimos en la línea 3 Let numeroUsuario y así creamos un bucle o ciclos repetitivo, para que el jugador siga jugando hasta que acierte, contrario a lo anterior que solo tenía una única oportunidad.

--- app.js
+++ app.js
@@ -1,20 +1,23 @@
 //Comentario 'ESTAS SON LAS VARIABLES//
-let numeroSecreto=4;    
-let numeroUsuario = prompt ("Me indicas un número entre 1 y 10 por favor:");
+let numeroSecreto=8;   
+let numeroUsuario=0;
+while (numeroUsuario != numeroSecreto){    
+    numeroUsuario = prompt ("Me indicas un número entre 1 y 10 por favor:");
 
-console.log(numeroUsuario);
-/*Este código realiza 
-la comparación
-*/
-if(numeroUsuario==numeroSecreto){
-    // Acertamos, fue verdadera la condición
-    alert(`Acertaste, el número es: ${numeroUsuario}`);
-} else {
-    if(numeroUsuario>numeroSecreto) {
-        alert('El número secreto es menor');
+    console.log(numeroUsuario);
+    /*Este código realiza 
+    la comparación
+    */
+    if(numeroUsuario==numeroSecreto){
+        // Acertamos, fue verdadera la condición
+        alert(`Acertaste, el número es: ${numeroUsuario}`);
     } else {
-        alert('El número secreto es mayor');
+        if(numeroUsuario>numeroSecreto) {
+            alert('El número secreto es menor');
+        } else {
+            alert('El número secreto es mayor');
+        }
+        //La condición no se cumplíó 
+        //alert ('Lo siento, no acertaste');
     }
-    //La condición no se cumplíó 
-    //alert ('Lo siento, no acertaste');
 }
