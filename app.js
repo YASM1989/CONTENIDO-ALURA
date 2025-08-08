a019af887dcb10ca95260e0e1ed1df390e92381c
YASM1989 <126576019+YASM1989@users.noreply.github.com>
2025-08-07 23:11:00 -0500

    Ingresamos lalínea 5 con  palabraIntentos, en la línea 16  ingresamos el Template String  (palabraIntento) y en la línea 24 afectamos el contador con palabraIntentos

--- app.js
+++ app.js
@@ -2,6 +2,8 @@
 let numeroSecreto=8;   
 let numeroUsuario=0;
 let intentos = 1; 
+let palabraIntentos='intento';
+
 while (numeroUsuario != numeroSecreto){    
     numeroUsuario = prompt ("Me indicas un número entre 1 y 10 por favor:");
 
@@ -11,14 +13,15 @@ while (numeroUsuario != numeroSecreto){
     */
     if(numeroUsuario==numeroSecreto){
         // Acertamos, fue verdadera la condición
-        alert(`Acertaste, el número es: ${numeroUsuario}.Lo lograste en ${intentos} intentos`);
+        alert(`Acertaste, el número es: ${numeroUsuario}.Lo lograste en ${intentos} ${palabraIntentos}`);
     } else {
         if(numeroUsuario>numeroSecreto) {
             alert('El número secreto es menor');
         } else {
             alert('El número secreto es mayor');
         }
-        intentos=intentos+1;
+        intentos=intentos+1
+        palabraIntentos ='intentos';
         //incrementamos el contador cuando no acierta 
         //La condición no se cumplíó 
         //alert ('Lo siento, no acertaste');
