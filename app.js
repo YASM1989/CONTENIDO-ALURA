f45dcdc4b7b7aa14a3973dcae9855bdc94d9f557
YASM1989 <126576019+YASM1989@users.noreply.github.com>
2025-08-08 19:54:18 -0500

    Ingresamos en línea 2, la función Math.floor(Math.random()*10)+1;  y la ´liean 8, console.log(numeroSecreto);

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 //Comentario 'ESTAS SON LAS VARIABLES//
-let numeroSecreto=8;   
+let numeroSecreto= Math.floor(Math.random()*10)+1;   
 let numeroUsuario=0;
 let intentos = 1; 
 //let palabraIntentos='intento';
 let maximosIntentos=3;
+
+console.log(numeroSecreto);
 
 while (numeroUsuario != numeroSecreto){    
     numeroUsuario = prompt ("Me indicas un número entre 1 y 10 por favor:");
